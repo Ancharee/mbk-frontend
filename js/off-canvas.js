85d9f9a6a52c05e8dbb7f8b491a19ec08021a0b8
Min <miiny27@gmail.com>
2020-11-12 22:12:49 +0700

    Add sidebar list

--- js/off-canvas.js
+++ js/off-canvas.js
@@ -4,5 +4,9 @@
     $('[data-toggle="offcanvas"]').on("click", function() {
       $('.sidebar-offcanvas').toggleClass('active')
     });
+    $('[data-toggle="offcanvas-list"]').on("click", function() {
+      $('.sidebar-list-offcanvas').toggleClass('active')
+      $('.main-panel').toggleClass('active');
+    });
   });
 })(jQuery);
