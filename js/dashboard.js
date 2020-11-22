1e9f846d559451261181be751701e28b3acb37d4
Min <miiny27@gmail.com>
2020-11-22 14:27:23 +0700

    Add pie chart

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,12 +2,55 @@
   'use strict';
   $(function() {
 
-    $('.circle').circleProgress({
-      size: 84,
-      thickness: 8,
-      startAngle: -Math.PI / 4 * 3,
-      lineCap: 'round',
-    });
+    if ($('.circle').length) {
+      $('.circle').circleProgress({
+        size: 84,
+        thickness: 8,
+        startAngle: -Math.PI / 4 * 3,
+        lineCap: 'round',
+      });
+    }
+
+    if ($("#pieChart").length) {
+      const datasets = $('#pieChart').data('sets');
+      const datasetsArray = datasets.split(',');
+
+      var doughnutPieData = {
+        datasets: [{
+          data: datasetsArray,
+          backgroundColor: [
+            '#4880FF',
+            '#FF5660',
+            '#FFC400',
+            '#B3C3FF',
+            '#24CCB8',
+          ]
+        }],
+      };
+
+      var doughnutPieOptions = {
+        responsive: true,
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        },
+        tooltips: {
+          enabled: false
+        },
+        elements: {
+          arc: {
+            borderWidth: 0
+          }
+        }
+      };
+
+      var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+      var pieChart = new Chart(pieChartCanvas, {
+        type: 'pie',
+        data: doughnutPieData,
+        options: doughnutPieOptions
+      });
+    }
 
   });
 })(jQuery);
