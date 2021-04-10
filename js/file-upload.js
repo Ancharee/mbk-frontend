d9b9dda0a0297ea48833f99f6dd68ccce24dfce2
Min <miiny27@gmail.com>
2021-04-10 15:37:48 +0700

    add comment page

--- js/file-upload.js
+++ js/file-upload.js
@@ -1,8 +1,14 @@
 (function($) {
   'use strict';
   $(function() {
-    $('.file-upload-default').on('change', function() {
+    $(document).on('change', '.file-upload-default', function () {
       $(this).prev().html($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+
+    $(document).on('click', '.file-upload-default-delete', function () {
+      const parent = $(this).parents('.form-group');
+      parent.find('input').val(null);
+      parent.find('label').html('');
     });
   });
 })(jQuery);
